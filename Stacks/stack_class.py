fb8c85b3088a5bb2d175d289dad6957f2f353f7b
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-11-21 20:29:21 +0330

    Update stack_class.py

--- Stacks/stack_class.py
+++ Stacks/stack_class.py
@@ -1,32 +1,43 @@
 class Stack:
     def __init__(self, max_size):
-        self.max_size = max_size  
+        self.max_size = max_size
         self.S = [0] * max_size  
-        self.num = 0  
-    
+        self.num = 0
+
     def push(self, item):
         if self.num >= self.max_size:
-            raise Exception("Stack overflow")
+            raise Exception("OverFlow")  
         self.S[self.num] = item
         self.num += 1
-        
-    def pop(self):
-        if self.num == 0:
-            raise Exception("Stack empty")
-        self.num -= 1
-        return self.S[self.num]
-    
-    def top(self):
-        if self.num == 0:
-            raise Exception("Stack empty")
-        return self.S[self.num-1]
 
-    
-    def size(self):
-        return self.num
-    
-    def is_full(self):
-        return self.num >= self.max_size
-    
+    def pop(self):
+        if self.num <= 0:
+            raise Exception("Empty")  
+        item = self.S[self.num - 1]
+        self.num -= 1
+        return item
+
     def is_empty(self):
-        return self.num == 0
+        if self.num == 0:
+            return True
+        else:
+            return False
+
+    def is_full(self):
+        if self.num >= self.max_size:
+            return True
+        else:
+            return False
+
+    def top(self):
+        if self.is_empty():
+            return "Stack is empty"
+        else:
+            return self.S[self.num - 1]
+
+    def display(self):
+        if self.is_empty():
+            return "Stack is empty"
+        while not self.is_empty():
+            print(self.pop())
+            
